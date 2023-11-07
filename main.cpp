51ddf4003c4c567fc6d924463404080adf92d4e0
csantillgar <csantill@myuax.com>
2023-11-07 11:39:29 +0100

    guardar

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 #include <iostream>
 using namespace std;
 int main(){
- for (int a = 1; a < 10; ++a){
-     for (int b = a ; b < 16; ++b){}
+ for (int a = 1; a < 9; ++a){
+     for (int b = a ; b < 16; ++b){
+         for (int c = b; c < 20; ++c){
+             if (a*a + b*b == c*c){
+                 cout << a << " " << b << " " << c << endl;
+             }
+         }
+     }
  }
 
 
